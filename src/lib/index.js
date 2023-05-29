0ded531fd2e4059fd7bd3a9a368beff1b745c615
TNThung <hung11112002@gmail.com>
2023-05-29 08:23:18 +0800

    [Fix] import

--- src/lib/index.js
+++ src/lib/index.js
@@ -1,5 +1,5 @@
 // Reexport your entry components here
-export { clickOut } from "../action/clickOut.js"
+export { clickOut } from "./action/clickOut.js"
 
-export { default as Divisor } from "../component/Divisor.svelte"
-export { default as Labeled } from "../component/Labeled.svelte"
+export { default as Divisor } from "./component/Divisor.svelte"
+export { default as Labeled } from "./component/Labeled.svelte"
