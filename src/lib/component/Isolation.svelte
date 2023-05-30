c8bb3709d45d70ba8c124866555842c3ae32a756
TNThung <hung11112002@gmail.com>
2023-05-30 08:57:17 +0800

    [Add] `Isolation` component

---
+++ src/lib/component/Isolation.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  export let color : string = "#0000";
+  export let zIndex: number = 1000;
+</script>
+
+
+<div style:color style:zIndex><slot/></div>
+
+
+<style lang="scss">
+  div {
+    inset    : 0;
+    position : fixed;
+    isolation: isolate;
+  }
+</style>
